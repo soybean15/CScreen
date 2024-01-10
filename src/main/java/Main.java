ad4cc03d6dad39a6f751178bfa54154d03beb1b3
Peter John Arao <pitzzahh@gmail.com>
2024-01-10 20:33:55 +0800

    :recycle: Optimized imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import tech.araopj.cscreen.components.CTable;
 import tech.araopj.cscreen.components.Label;
 import tech.araopj.cscreen.components.Screen;
 import tech.araopj.cscreen.components.TextBox;
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class Main {
